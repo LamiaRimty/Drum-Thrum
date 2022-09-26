7e298b34dee71b82eeca4905dab59f40fb7f4d9c
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-09-26 21:32:36 +0200

    Add EventListener

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 
-var numberOfButtons=document.querySelectorAll(".drum").length;
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;
 
-for(var i=0 ; i<numberOfButtons; i++ ){
+for(var i=0 ; i<numberOfDrumButtons; i++ ){
 document.querySelectorAll(".drum")[i].addEventListener("click",function respondToclick()
 {
-    console.log("I got clicked!");
+    var buttonInnerHTML=this.html;
+    switch (buttonInnerHTML) {
+        case "a":
+            
+            break;
+    
+        default:
+            break;
+    }
+   
+    
 }
 );
 }
+//this.style.color="red";
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
