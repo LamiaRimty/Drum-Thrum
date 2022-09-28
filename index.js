dc0ca85b3d2d55e965d66de4efecdfe1fa990893
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-09-28 21:39:56 +0200

    Add audio

--- index.js
+++ index.js
@@ -2,22 +2,10 @@
 var numberOfDrumButtons=document.querySelectorAll(".drum").length;
 
 for(var i=0 ; i<numberOfDrumButtons; i++ ){
-document.querySelectorAll(".drum")[i].addEventListener("click",function respondToclick()
+document.querySelectorAll(".drum")[i].addEventListener("click",function ()
 {
-    var buttonInnerHTML=this.html;
-    switch (buttonInnerHTML) {
-        case "a":
-            
-            break;
-    
-        default:
-            break;
-    }
+    alert("I go clicked");
    
-    
+           
+    });
 }
-);
-}
-//this.style.color="red";
-// var audio = new Audio('sounds/tom-1.mp3');
-// audio.play();
