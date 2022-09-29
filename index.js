aea4fcdeea3b5ec5294d9335ff4487fabc8e8e6a
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-09-29 19:33:21 +0200

    Add keyboard event

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
-
+//Detecting button press
 var numberOfDrumButtons=document.querySelectorAll(".drum").length;
 
 for(var i=0 ; i<numberOfDrumButtons; i++ ){
-document.querySelectorAll(".drum")[i].addEventListener("click",function ()
+document.querySelectorAll(".drum")[i].addEventListener("click",function()
 {
-    alert("I go clicked");
+
+    var buttonInnerHTML=this.innerHTML;
+    
+    makeSound(buttonInnerHTML);
+
+});}
+
+
+//Detecting keyboard press
+document.addEventListener("keypress",function(event){
+ makeSound(event.key);
    
-           
-    });
-}
+});
+
+function makeSound(key){
+
+
+    switch (key) {
+        case "a":
+        var tom1=new Audio("sounds/tom-1.mp3");
+         tom1.play();   
+          break;
+     
+          case "b":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();   
+             break;
+
+             case "c":
+             var tom3=new Audio("sounds/tom-3.mp3");
+             tom3.play();   
+              break;
+
+              case "d":
+              var tom4=new Audio("sounds/tom-4.mp3");
+              tom4.play();   
+               break;
+
+               case "x":
+               var snare=new Audio("sounds/snare.mp3");
+               snare.play();   
+                break;
+
+                case "y":
+                var crash=new Audio("sounds/crash.mp3");
+                crash.play();   
+                 break;
+
+                 case "z":
+                 var kick=new Audio("sounds/kick-bass.mp3");
+                 kick.play();   
+                  break;
+
+    
+        default:console.log(buttonInnerHTML);
+    }
+    }
+
